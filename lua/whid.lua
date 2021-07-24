46a0b2ed62eaa3c1dfd3f8349351f2942de257df
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-07-24 10:30:25 +0900

    whid.lua: center

--- lua/whid.lua
+++ lua/whid.lua
@@ -75,3 +75,9 @@ local function update_view(direction)
   api.nvim_buf_set_option(buf, 'modifiable', false)
 end
 
+local function center(str)
+  local width = api.nvim_win_get_width(0)
+  local shift = math.floor(width / 2) - math.floor(string.len(str) / 2)
+  return string.rep(' ', shift) .. str
+end
+
